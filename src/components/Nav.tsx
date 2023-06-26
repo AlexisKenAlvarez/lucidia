0e32e9d403824a2426e9d5dc641a1b1f5da356c4
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-06-26 21:21:53 +0800

    responsive hero

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -24,13 +24,13 @@ const Nav = () => {
     ]
 
     return (
-        <nav className="w-full h-auto py-4 absolute top-0 left-0 z-20 px-5">
-            <img src="/hero/navline.webp" alt="NavLine" className="w-full absolute left-0 top-7 right-0" />
+        <nav className="w-full h-auto px-5 xl:py-4 py-0  absolute top-0 left-0 z-20">
+            <img src="/hero/navline.webp" alt="NavLine" className="w-full absolute left-0 top-7 right-0 xl:block hidden" />
 
             <div className="max-w-[1400px] w-full mx-auto flex justify-between items-center mt-5">
                 <img src="/hero/logo.webp" alt="Logo" className="" />
 
-                <ul className="font-secondary font-semibold text-sm uppercase flex items-center gap-x-12 text-white">
+                <ul className="font-secondary font-semibold text-sm uppercase items-center gap-x-12 text-white xl:flex hidden">
                     {navList.map((items, i) => {
                         return (
                             <li className="" key={i}>{items.name}</li>
@@ -44,7 +44,7 @@ const Nav = () => {
                         <div className="w-6 h-[3px] bg-white"></div>
                     </button>
 
-                    <button className="w-[13rem] h-9 clipped bg-bl relative  font-bold flex items-center justify-center">
+                    <button className="w-[13rem] h-9 clipped bg-bl relative  font-bold items-center justify-center md:flex hidden">
                         <div className="absolute bg-[#5e38b2] w-[98%] h-[91%] left-0 top-0 right-0 bottom-0 m-auto clipped"></div>
                         <h1 className="font-secondary relative z-10 text-bl text-[12px]">MEET THE COMMUNITY</h1>
                     </button>
--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -26,15 +26,15 @@ const Hero = () => {
     ]
 
     return (
-        <section className="w-full h-screen px-5 py-10 relative text-white flex items-center justify-center">
+        <section className="w-full min-h-screen p-5 relative text-white flex md:items-center justify-center">
             <img src="/hero/herobg.webp" alt="Herobg" className="w-full h-full object-cover absolute top-0 left-0" />
 
-            <div className="max-w-[1400px] w-full h-auto mx-auto relative z-10 flex mt-[10rem]">
+            <div className="max-w-[1400px] w-full h-auto mx-auto relative z-10 flex mt-[7rem] md:mt-[10rem] flex-col-reverse md:flex-row">
 
                 <div className="w-full">
-                    <h1 className="font-primary text-[72px] max-w-[40rem] leading-[5.5rem] font-semibold">AI DRIVEN GAMEFI GOVERNED BY YOU</h1>
+                    <h1 className="font-primary text-3xl md:text-5xl 2xl:text-[72px] max-w-[40rem] 2xl:leading-[5.5rem] font-semibold md:block hidden">AI DRIVEN GAMEFI GOVERNED BY YOU</h1>
 
-                    <div className=" max-w-[37rem] font-primary font-medium flex flex-col gap-y-5">
+                    <div className=" max-w-[37rem] font-primary font-medium flex flex-col gap-y-5 md:block md:text-base text-sm">
                         <p className="">We are not looking to reinvent the wheel of Gamification and game plots - just to give you the power and ownership over what you have already been doing for years.</p>
 
                         <p className="">
@@ -42,13 +42,13 @@ const Hero = () => {
                         </p>
                     </div>
 
-                    <div className="flex gap-x-3 mt-10">
-                        <button className="w-[9rem] h-14 clipped2 bg-cyan relative  font-bold flex items-center justify-center scale-x-[-1]">
+                    <div className="flex gap-x-3 mt-10 md:flex-row flex-col gap-y-5">
+                        <button className=" w-full md:w-[9rem] h-14 clipped2 bg-cyan relative  font-bold flex items-center justify-center scale-x-[-1]">
                             <div className="absolute bg-cyan w-[98%] h-[91%] left-0 top-0 right-0 bottom-0 m-auto clipped2"></div>
                             <h1 className="font-secondary relative z-10 text-black font-[700] text-[12px] scale-x-[-1]">BUY $LUCID</h1>
                         </button>
 
-                        <button className="w-[13rem] h-14 clipped2 bg-white relative  font-bold flex items-center justify-center scale-x-[-1]">
+                        <button className=" w-full md:w-[13rem] h-14 clipped2 bg-white relative  font-bold flex items-center justify-center scale-x-[-1]">
                             <div className="absolute bg-[#2e194c] w-[98%] h-[96%] left-0 top-0 right-0 bottom-0 m-auto clipped2">
                                 <img src="/hero/trans.webp" alt="Herobg" className="w-full h-full object-cover absolute top-0 left-0" />
 
@@ -57,7 +57,7 @@ const Hero = () => {
                         </button>
                     </div>
 
-                    <div className="mt-20 flex items-center gap-x-8">
+                    <div className="mt-20 flex md:items-center gap-x-8 md:flex-row flex-col gap-y-9">
                         <div className="">
                             <h1 className="font-primary font-medium">Join community of 100k+ Lucidians</h1>
                             <ul className="flex items-center gap-x-4 text-2xl mt-4">
@@ -70,7 +70,7 @@ const Hero = () => {
                                 })}
                             </ul>
                         </div>
-                        <div className="h-16 w-[1px] bg-white/10"></div>
+                        <div className="h-16 w-[1px] bg-white/10 md:block hidden"></div>
                         <div className="">
                             <h1 className="font-primary font-medium">Audited by:</h1>
                             <ul className="flex items-center gap-x-7 text-2xl mt-4">
@@ -84,8 +84,9 @@ const Hero = () => {
                     </div>
                 </div>
 
-                <div className="w-full relative">
-                    <img src="/hero/hero.webp" alt="Hero" className="object-cover top-0 -mt-4 h-[70vh] mx-auto" />
+                <div className="w-full relative flex">
+                    <h1 className="font-primary text-3xl md:text-5xl 2xl:text-[72px] max-w-[40rem] 2xl:leading-[5.5rem] font-semibold md:hidden block">AI DRIVEN GAMEFI GOVERNED BY YOU</h1>
+                    <img src="/hero/hero.webp" alt="Hero" className="object-cover top-0 md:w-auto h-[35vh] -mt-4 md:h-[70vh] mx-auto " />
                 </div>
 
             </div>
