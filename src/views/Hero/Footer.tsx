09b7364df41f48b319f147507cd3d9fa93e2e10b
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-06-28 13:02:22 +0800

    finished footer

--- src/views/Hero/Footer.tsx
+++ src/views/Hero/Footer.tsx
@@ -57,15 +57,16 @@ const Footer = () => {
 
     return (
         <footer className="w-full h-auto min-h-screen relative pb-16">
-            <div className="max-w-[1400px] mx-auto w-full text-white mt-28">
-                <div className="text-center mt-10">
-                    <h1 className="text-[46px] font-semibold font-primary bg-clip-text bg-gradient-to-r w-fit mx-auto from-white via-white to-[#c4f4fe] text-transparent">
+
+            <div className="max-w-[1400px] mx-auto w-full text-white sm:mt-28 mt-10 px-5 text-left sm:text-center">
+                <div className="mt-10">
+                    <h1 className="sm:text-[46px] text-[32px] font-semibold font-primary bg-clip-text bg-gradient-to-r w-fit sm:mx-auto  from-white via-white to-[#c4f4fe] text-transparent">
                         Join community of 100k+ Lucidians
                     </h1>
-                    <p className="font-medium font-primary text-footergry text-[22px]">Fore more details about our progress and workflow be sure to follow</p>
+                    <p className="font-medium font-primary text-footergry text-[15px] sm:text-[22px] mt-2 sm:mt-0">For more details about our progress and workflow be sure to follow</p>
                 </div>
 
-                <ul className="flex w-fit mx-auto mt-10 text-4xl gap-x-7">
+                <ul className="flex w-fit sm:mx-auto mt-12 text-2xl sm:text-4xl sm:gap-x-7 gap-x-5">
                     {icons.map((items, i) => {
                         return (
                             <li className="" key={i}>
@@ -79,13 +80,13 @@ const Footer = () => {
 
             </div>
 
-            <img src="/hero/navline.webp" alt="navline" className="mt-28 mx-auto w-full object-cover relative left-0 right-0 opacity-70" />
+            <img src="/hero/navline.webp" alt="navline" className="sm:mt-28 mx-auto w-full object-cover relative left-0 right-0 opacity-70 sm:block hidden" />
 
-            <div className="max-w-[1400px] mx-auto w-full text-white mt-28 flex justify-between">
-                <div className="">
+            <div className="max-w-[1400px] mx-auto w-full text-white sm:mt-28 flex justify-between px-5 lg:pr-12 gap-x-10 lg:flex-row flex-col sm:border-t-0 border-t-[1.5px] mt-10 border-white/20">
+                <div className="mt-10 sm:mt-0">
                     <img src="/hero/logo.webp" alt="Logo" className="" />
                     <p className="font-primary font-medium max-w-[33rem] mt-7">YOUR JURISDICTION MAY NOT REGULATE CRYPTOCURRENCIES. THE VALUE OF CRYPTOCURRENCIES MAY RISE AND FALL. PROFITS MAY BE SUBJECT TO CAPITAL GAINS TAXES OR OTHER LOCAL TAXES.</p>
-                    <div className="text-footergry flex items-center gap-x-16 mt-5">
+                    <div className="text-footergry flex items-center gap-x-7 sm:gap-x-16 mt-5">
                         <div className="flex items-center gap-x-[5px]">
                             <AiOutlineMail className="text-white text-2xl" />
                             <a href="mailto:hello@lucidia.io" className="font-medium font-primary">
@@ -100,13 +101,13 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="font-primary flex gap-x-20">
+                <div className="font-primary flex gap-x-20 lg:flex-row flex-col gap-y-8 mt-11 lg:mt-0">
                     <div className="">
                         <h2 className="font-semibold text-[22px]">GENERAL</h2>
                         <ul className="flex flex-col gap-y-2 mt-8">
                             {general.map((items, i) => {
                                 return (
-                                    <li className="font-secondary font-semibold text-sm text-footergry relative w-fit" key={i}>
+                                    <li className="font-secondary font-semibold text-sm text-footergry relative w-fit hover:text-white transition-colors ease-in-out duration-300" key={i}>
                                         <div className={`w-7 h-3 bg-grn absolute -right-9 top-0 bottom-0 my-auto flex items-center justify-center ${i === 0 ? 'block' : 'hidden'}`}>
                                             <h5 className="text-[8px] font-primary font-bold text-center text-black mt-[2px]">NEW</h5>
                                         </div>
@@ -124,7 +125,7 @@ const Footer = () => {
                         <ul className="flex flex-col gap-y-2 mt-8">
                             {company.map((items, i) => {
                                 return (
-                                    <li className="font-secondary font-semibold text-sm text-footergry relative w-fit uppercase" key={i}>
+                                    <li className="font-secondary font-semibold text-sm text-footergry relative w-fit uppercase hover:text-white transition-colors ease-in-out duration-300" key={i}>
                                         <div className={`w-7 h-3 bg-cyan absolute -right-9 top-0 bottom-0 my-auto flex items-center justify-center ${i === 2 ? 'block' : 'hidden'}`}>
                                             <h5 className="text-[8px] font-primary font-bold text-center text-black mt-[2px]">NEW</h5>
                                         </div>
@@ -141,16 +142,16 @@ const Footer = () => {
 
             </div>
 
-            <img src="/hero/navline.webp" alt="navline" className="mt-10 mx-auto w-full object-cover relative left-0 right-0 opacity-70" />
+            <img src="/hero/navline.webp" alt="navline" className="mt-10 mx-auto w-full object-cover relative left-0 right-0 opacity-70 sm:block hidden" />
 
-            <div className="max-w-[1400px] mx-auto w-full text-white mt-28 flex justify-between items-center">
+            <div className="max-w-[1400px] mx-auto w-full text-white sm:mt-16 flex justify-between items-center px-5 sm:border-t-0 border-t-[1.5px] mt-20 pt-12 sm:pt-0 border-white/20">
                 <div className="">
                     <h3 className="font-secondary text-sm font-semibold uppercase">© Lucidia 2023 | All rights reserved.</h3>
                     <p className="text-[12px] text-footergry font-secondary mt-2">YOUR JURISDICTION MAY NOT REGULATE CRYPTOCURRENCIES. THE VALUE OF CRYPTOCURRENCIES MAY RISE AND FALL. PROFITS MAY BE SUBJECT TO CAPITAL GAINS TAXES OR OTHER LOCAL TAXES.
                     </p>
                 </div>
 
-                <button className="" onClick={handleScrollTop}>
+                <button className="sm:block hidden flex-shrink-0" onClick={handleScrollTop}>
                     <img src="/iconup.webp" alt="Icon" className="w-8 h-8" />
                 </button>
 
